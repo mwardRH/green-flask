b7bc0eb44845ec2856992835bac2abf82623800b
Matthew Ward <mward@earth.lab>
2017-06-01 15:30:58 -0400

    Total retool

--- wsgi.py
+++ wsgi.py
@@ -1,15 +1,9 @@
-import os
-import httpd
-#
-# IMPORTANT: Put any additional includes below this line.  If placed above this
-# line, it's possible required libraries won't be in your searchable path
-#
+from flask import Flask
+application = Flask(__name__)
 
-from flaskapp import app as application
+@application.route("/")
+def hello():
+    return "Hello BLUE!"
 
-#
-# Below for testing only
-#
-virtenv = os.path.join(os.environ.get('OPENSHIFT_PYTHON_DIR','.'), 'virtenv')
-
-httpd.serve_forever()
+if __name__ == "__main__":
+    application.run()
